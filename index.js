2c82e1bfc58a931e54e218386c92a747f81ac2f3
karsyn ansari <karsyn.ansari@gmail.com>
2021-12-04 14:56:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.setAttribute = "h1#victory";
+newHeader.innerHTML = "Karsyn is the champion";
